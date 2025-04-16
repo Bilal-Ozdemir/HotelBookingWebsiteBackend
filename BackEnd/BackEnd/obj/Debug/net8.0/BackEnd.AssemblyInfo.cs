dce00d42b558f206f54f1281e74146bace15788f
Sayed5zan <faizansulaiman90@gmail.com>
2025-04-16 18:45:02 -0400

    some css changes

--- BackEnd/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95896c8897372198afd3dd5f7b18ae4ecd870f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17ed9782d0a6d754a736dd864c2ed5eb28aa114")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
